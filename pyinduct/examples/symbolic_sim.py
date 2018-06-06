a1a5dba97ff9d2f41c4e9e19eb5584110a2b2e29
Stefan Ecklebe <stefan.ecklebe@tu-dresden.de>
2018-06-06 21:17:35 +0200

    Further work on symbolic interface, nearly at state space representation

--- pyinduct/examples/symbolic_sim.py
+++ pyinduct/examples/symbolic_sim.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pyinduct as pi
 import pyinduct.sym_simulation as ss
 import sympy as sp
@@ -6,7 +7,8 @@ import sympy as sp
 import matplotlib.pyplot as plt
 
 # approximation order
-N = 10
+N = 3
+spat_bounds = (0, 1)
 
 # define temporal and spatial variables
 t = ss.time
@@ -14,13 +16,14 @@ z = ss.space[0]
 # z1, z2 = ss.space[:2]
 
 # define symbols of spatially distributed and lumped system variables
-x1 = sp.symbols("x1", cls=sp.Function)
-x2 = sp.symbols("x1", cls=sp.Function)
-gamma = sp.symbols("gamma", cls=sp.Function)
+x1 = sp.symbols("x1", cls=sp.Function)(z, t)
+x2 = sp.symbols("x1", cls=sp.Function)(z, t)
+gamma = sp.symbols("gamma", cls=sp.Function)(t)
 
 # define symbols for test functions
 # phi = sp.symbols("phi1", cls=sp.Function)
-phi = sp.symbols("phi1(:{})".format(N), cls=sp.Function)
+phi = [sp.symbols("phi_{}".format(i), cls=sp.Symbol)(z)
+       for i in range(N)]
 print(phi)
 
 # define system inputs
@@ -28,11 +31,12 @@ u1 = sp.symbols("u1", cls=sp.Function)
 u2 = sp.symbols("u2", cls=sp.Function)
 
 # define parameters
-alpha, Gamma, k, rho, L = sp.symbols(("alpha:2",
-                                      "Gamma:2",
-                                      "k:2",
-                                      "rho:2",
-                                      "L"))
+alpha, Gamma, k, rho, L, Tm = sp.symbols(("alpha:2",
+                                          "Gamma:2",
+                                          "k:2",
+                                          "rho:2",
+                                          "L",
+                                          "T_m"), real=True)
 
 equations = []
 
@@ -40,16 +44,15 @@ equations = []
 for idx, (x, u) in enumerate(zip([x1, x2], [u1, u2])):
     weak_forms = []
     for p in phi:
-        beta = (gamma(t) - Gamma[idx])
+        beta = (gamma - Gamma[idx])
         exp = (
-            ss.InnerProduct(x(z, t).diff(t), p(z))
+            ss.InnerProduct(x.diff(t), p, spat_bounds)
             - alpha[idx] / beta**2 * (
-                (x(z, t).diff(z) * p(z)).subs(z, 1)
-                - beta / k[idx] * u(t) * p(0)
-                - ss.InnerProduct(x(z, t).diff(z), p(z).diff(z))
+                (x.diff(z) * p).subs(z, 1)
+                - beta / k[idx] * u(t) * p.subs(z, 0)
+                - ss.InnerProduct(x.diff(z), p.diff(z), spat_bounds)
             )
-            - gamma(t).diff(t) / beta * ss.InnerProduct(z*x1(z, t).diff(z),
-                                                        p(z))
+            - gamma.diff(t) / beta * ss.InnerProduct(z*x.diff(z), p, spat_bounds)
         )
         # sp.pprint(exp)
         # quit()
@@ -58,24 +61,38 @@ for idx, (x, u) in enumerate(zip([x1, x2], [u1, u2])):
 
 # define the ode for the phase boundary
 g_exp = 1/(rho[1]*L)*(
-    k[0]/(gamma(t) - Gamma[0]) * x1(z, t).diff(z).subs(z, 1)
-    - k[1]/(gamma(t) - Gamma[1]) * x2(z, t).diff(z).subs(z, 1)
+    k[0]/(gamma - Gamma[0]) * x1.diff(z).subs(z, 1)
+    - k[1]/(gamma - Gamma[1]) * x2.diff(z).subs(z, 1)
 )
 # sp.pprint(g_exp)
 equations.append(g_exp)
 
 # approximations
-jac = sp.jacobi(5, 1, 1, z)
-sp.plot(jac)
+fem_base = ss.create_lag1ast_base(z, spat_bounds, N)
+x1_approx = ss.create_approximation(z, fem_base, ess_bounds=[(1, Tm)])
+x2_approx = ss.create_approximation(z, fem_base, ess_bounds=[(1, Tm)])
+gamma_approx = ss.get_weights(1)[0]
+# print(x1_approx)
+# print(x1_approx)
+# print(x1_approx.atoms(sp.Function))
+# print(gamma_approx.atoms(sp.Function))
+
+# specify replacement dicts
+subs_list = [(x1, x1_approx),
+             (x2, x2_approx),
+             (gamma, gamma_approx),
+             ] + list(zip(phi, fem_base))
+s_eqs = [eq.subs(subs_list).doit() for eq in equations]
+print([eq for eq in s_eqs])
+
+# aquire targets
+targets = set()
+for eq in s_eqs:
+    targets.update(eq.atoms(sp.Derivative))
+
+print(targets)
+
+res = sp.solve(s_eqs, targets, dict=True)
+print(res)
 
 
-
-# define base
-diri_boundary = z- 1
-base = ss.create_orthogonal_base(sp.polys.orthopolys.legendre_poly,
-                                 N,
-                                 z
-                                 (0, 1),
-                                 diri_boundary)
-sp.pprint(base[1])
-sp.plot(base[1], (z, 0, 1))
--- pyinduct/sym_simulation.py
+++ pyinduct/sym_simulation.py
@@ -6,8 +6,23 @@ import sympy as sp
 from sympy.functions.special.polynomials import jacobi
 import numpy as np
 
-time = sp.symbols("t")
-space = sp.symbols("z:3")
+from .core import Domain
+
+time = sp.symbols("t", real=True)
+space = sp.symbols("z:3", real=True)
+
+
+_symbol_cnt = 0
+
+
+def get_weights(n):
+    cnt = getattr(get_weights, "_cnt", 0)
+    if n < 0:
+        raise ValueError
+    c = [sp.symbols("c{}".format(cnt + i), real=True, cls=sp.Function)(time)
+         for i in range(n)]
+    setattr(get_weights, "_cnt", cnt + n)
+    return c
 
 
 class FieldVariable(sp.Function):
@@ -24,7 +39,7 @@ class InnerProduct(sp.Expr):
     """
     is_complex = True
 
-    def __new__(cls, left, right):
+    def __new__(cls, left, right, bounds):
         # identify free variables of arguments
         vars = left.atoms(sp.Symbol).union(right.atoms(sp.Symbol))
 
@@ -34,7 +49,7 @@ class InnerProduct(sp.Expr):
         # construct integrals over remaining coordinates
         exp = sp.conjugate(left) * right
         for dim in vars:
-            exp = sp.Integral(exp, dim)
+            exp = sp.Integral(exp, (dim, bounds[0], bounds[1]))
         return exp
 
     @property
@@ -49,28 +64,80 @@ class InnerProduct(sp.Expr):
         pass
 
 
-def create_approximation(cls,
-                           order,
-                           symbol,
-                           domain,
-                           ess_boundary=None,
-                           nat_boundary=None):
+class Lagrange1stOrder(sp.Piecewise):
+
+    def __new__(cls, start, mid, end):
+        x = sp.Symbol("x")
+        obj = sp.Piecewise.__new__(cls, (0, x < start),
+                                   ((x-start)/(mid - start), x < mid),
+                                   (1, x == mid),
+                                   (1 - (x-mid)/(end - mid), x < end),
+                                   (0, x >= end),
+                        )
+        return obj
+
+
+def build_lag1st(sym, start, mid, end):
+    if start == mid:
+        obj = sp.Piecewise((0, sym < mid),
+                           (1 - (sym - mid)/(end - mid), sym < end),
+                           (0, sym >= end)
+                           )
+    elif mid == end:
+        obj = sp.Piecewise((0, sym < start),
+                           ((sym - start)/(mid - start), sym <= mid),
+                           (0, sym > mid)
+                           )
+    else:
+        obj = sp.Piecewise((0, sym < start),
+                           ((sym - start)/(mid - start), sym < mid),
+                           (1, sym == mid),
+                           (1 - (sym - mid)/(end - mid), sym < end),
+                           (0, sym >= end)
+                           )
+    return obj
+
+
+def create_lag1ast_base(sym, bounds, num):
+    # define base vectors
+    nodes = Domain(bounds, num)
+    funcs = [build_lag1st(sym, *nodes[[0, 0, 1]])]
+    for idx in range(1, num-1):
+        funcs.append(build_lag1st(sym, *nodes[[idx-1, idx, idx+1]]))
+    funcs += [build_lag1st(sym, *nodes[[-2, -1, -1]])]
+
+    if 0:
+        # plotting of Piecewise fails really bad, so lambdify first
+        fig = plt.figure()
+        vals = np.linspace(0, 1, num=1e3)
+        for f in funcs:
+            fn = sp.lambdify(z, f, modules="numpy")
+            res = fn(vals)
+            plt.plot(vals, res)
+        plt.show()
+
+    return funcs
+
+
+def create_approximation(sym, funcs, ess_bounds):
     """
     Create an approximation basis.
-
-
-
-    Args:
-        order:
-        domain:
-        ess_boundary:
-        nat_boundary:
-
-    Returns:
-
     """
-    # if cls not in ortho_bases:
-    #     raise ValueError("Unknown generator")
+    num = len(funcs)
+    # extract shape function that is to be eliminated
+    nat_idxs = []
+    for db in ess_bounds:
+        idx = next((idx for idx in range(num)
+                    if funcs[idx].subs(sym, db[0]) == 1))
+        nat_idxs.append(idx)
 
-    base = [cls(n, x=symbol, polys=True) for n in range(order)]
-    return base
+    nat_funcs = [funcs[idx] for idx in range(num) if idx not in nat_idxs]
+    ess_funcs = [funcs[idx] for idx in range(num) if idx in nat_idxs]
+
+    # define approximated system variable
+    c = get_weights(num - len(ess_bounds))
+    x_ess = [db[1] * ef for db, ef in zip(ess_bounds, ess_funcs)]
+    x_nat = [c * f for c, f in zip(c, nat_funcs)]
+
+    return sp.Add(*x_ess) + sp.Add(*x_nat)
+
